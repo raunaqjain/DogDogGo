3930f305560684898a2c2c697704cc620f113c46
blogitraveller <blogitraveller@gmail.com>
2020-02-29 17:37:03 -0500

    new change

--- search/views.py
+++ search/views.py
@@ -206,7 +206,7 @@ class SearchQueryView(APIView):
         # contants
         core_name = "DDG"
         select_q = "/select?q="
-        localhost = "http://18.191.146.199:8983/solr/" + core_name + select_q
+        localhost = "http://18.222.24.164:8983/solr/" + core_name + select_q
         highlight_search = "&hl.fl=full_text,text_*&hl=on&hl.simple.pre=%3Cspan%20class%3D%22tweet-hl%22%3E&" \
                            "hl.simple.post=%3C%2Fspan%3E"
         custom_search = "&defType=edismax&pf=processed_text%5E2&ps=5&hl.fragsize=300" + highlight_search
